e6a792df15d893e537de63bcb7d3575d06911914
Edilson Pantoja <edilsonpantoja@hotmail.com>
2022-02-10 13:46:30 -0500

    Transforming Views into Classes

--- movie/urls.py
+++ movie/urls.py
@@ -1,10 +1,11 @@
 # url - view - template
 
 from django.urls import path, include
-from .views import homepage, homefilmes
+from .views import Homefilmes, Homepage
 
 
 urlpatterns = [
-    path('', homepage),
-    path('movies/', homefilmes)
+    #path('', homepage),
+    path('', Homepage.as_view()),
+    path('movies/', Homefilmes.as_view()),
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render
 from .models import Movie
+from django.views.generic import TemplateView, ListView
+
+
+class Homepage(TemplateView):
+    template_name = "homepage.html"
+
+
+class Homefilmes(ListView):
+    template_name = "homefilmes.html"
+    model = Movie
+    #object_list
+    #o object_list vai ser passado pro html com a lista dos dados do Modelo Movie
+    #quando se usa classe nao precisa passar context automaticamente o django gerencia varias coisas
+
 
 # Create your views here.
-def homepage(request):
-    return render(request, "homepage.html")
+#def homepage(request):
+#    return render(request, "homepage.html")
 
 
 #url - view - html
-def homefilmes(request):
-    """
-    Criar um context pra passar como terceiro argumento na funcao render
-    Criar um dicionario, uma lista que vai conter informacoes do database
-    para ser passado na funcao
-
-    lista_filmes eh a chave do dicionario que eh passada com o mesmo nome da variavel
-    """
-    context = {}
-    lista_filmes = Movie.objects.all()
-    context['lista_filmes'] =  lista_filmes
-    return render(request, "homefilmes.html", context)
+# def homefilmes(request):
+#     """
+#     Criar um context pra passar como terceiro argumento na funcao render
+#     Criar um dicionario, uma lista que vai conter informacoes do database
+#     para ser passado na funcao
+#
+#     lista_filmes eh a chave do dicionario que eh passada com o mesmo nome da variavel
+#     """
+#     context = {}
+#     lista_filmes = Movie.objects.all()
+#     context['lista_filmes'] =  lista_filmes
+#     return render(request, "homefilmes.html", context)
