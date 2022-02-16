b596b0f20e10b63d69cf159efb269a63c41fe8dc
Edilson Pantoja <edilsonpantoja@hotmail.com>
2022-02-16 15:45:26 -0500

    Editing Profile and Changing User Password

--- movie/urls.py
+++ movie/urls.py
@@ -1,6 +1,5 @@
 # url - view - template
-
-from django.urls import path, include
+from django.urls import path, reverse_lazy
 # importando os arquivos .html
 from .views import Homefilmes, Homepage, Detalhesfilme, Pesquisafilme, Paginaperfil, Criarcontas
 from django.contrib.auth import views as auth_view
@@ -16,6 +15,10 @@ urlpatterns = [
     # Django ja tem uma template pra login, precisa ser importada, ver auth_view acima
     path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('editarperfil/', Paginaperfil.as_view(), name='editarperfil'),
+    path('editarperfil/<int:pk>', Paginaperfil.as_view(), name='editarperfil'),
     path('criarconta/', Criarcontas.as_view(), name='criarconta'),
+    # abaixo da pra aproveitar o mesmo html usado para alterar o perfil
+    # usamos reverse_lazy pq aqui estamos passando como parametro no path
+    path('mudarsenha/', auth_view.PasswordChangeView.as_view(template_name='editarperfil.html',
+                                                             success_url=reverse_lazy('movie:homefilmes')), name='mudarsenha'),
 ]
